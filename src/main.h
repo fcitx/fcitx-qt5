7bae6eab6a6e64e3f59bf8b52d0a98fae2f0bb05
Weng Xuetian <wengxt@gmail.com>
2012-04-17 21:11:00 +0800

    add missing file

---
+++ src/main.h
@@ -0,0 +1,14 @@
+#include <QPlatformInputContext>
+#include <private/qplatforminputcontextplugin_qpa_p.h>
+#include <QtCore/QStringList>
+
+#include "qfcitxplatforminputcontext.h"
+
+class QFcitxPlatformInputContextPlugin : public QPlatformInputContextPlugin
+{
+    Q_OBJECT
+public:
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QPlatformInputContextFactoryInterface" FILE "fcitx.json")
+    QStringList keys() const;
+    QFcitxPlatformInputContext *create(const QString& system, const QStringList& paramList);
+};
