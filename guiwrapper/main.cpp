721c28b4c64d6d8727da197e0b4927e3a6224967
Weng Xuetian <wengxt@gmail.com>
2017-02-04 10:50:10 -0800

    [qt5] fix build and add support for webengine

--- guiwrapper/main.cpp
+++ guiwrapper/main.cpp
@@ -25,6 +25,7 @@
 
 int main(int argc, char* argv[])
 {
-  WrapperApp app(argc, argv);
-  return app.exec();
+    QGuiApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+    WrapperApp app(argc, argv);
+    return app.exec();
 }
