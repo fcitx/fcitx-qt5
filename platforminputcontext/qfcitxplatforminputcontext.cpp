bf31789fbe773a554fe33205e817ca11e3ef238c
Weng Xuetian <wengxt@gmail.com>
2020-05-26 15:07:31 -0700

    Consider frame margin for relative rect on wayland.

--- platforminputcontext/qfcitxplatforminputcontext.cpp
+++ platforminputcontext/qfcitxplatforminputcontext.cpp
@@ -322,6 +322,8 @@ void QFcitxPlatformInputContext::cursorRectChanged() {
     }
 
     if (data.capability & CAPACITY_RELATIVE_CURSOR_RECT) {
+        auto margins = inputWindow->frameMargins();
+        r.translate(margins.left(), margins.top());
         if (data.rect != r) {
             data.rect = r;
             proxy->setCursorRect(r.x(), r.y(), r.width(), r.height());
